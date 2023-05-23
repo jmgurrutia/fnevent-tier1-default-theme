d0095f0b15285638ad737b36775c53d8024d23e1
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2023-05-23 13:21:40 -0300

    Initial commit.

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  /*flags: {
+    DEV_SSR: true
+  },*/
+  siteMetadata: {
+    title: "Tier 1 Default",
+    siteUrl: "https://tier-1-default.fnvirtual.app",
+  },
+  plugins: [
+    "@openeventkit/event-site"
+  ]
+};
