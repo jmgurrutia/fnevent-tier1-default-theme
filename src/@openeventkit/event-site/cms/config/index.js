9a9e5bfb1aaf3fa0ef7a56dd5b6db7ed0c070ce2
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2023-05-23 13:51:43 -0300

    Removes NetlifyCMS local backend config.

--- src/@openeventkit/event-site/cms/config/index.js
+++ src/@openeventkit/event-site/cms/config/index.js
@@ -3,8 +3,5 @@ import defaultConfig from "@openeventkit/event-site/src/cms/config";
 const config = { ...defaultConfig };
 // TODO: move to env var
 config.backend.repo = "fntechgit/fnevent-tier1-default-theme";
-config.local_backend = {
-  url: "http://localhost:8081/api/v1"
-};
 
 export default config;
