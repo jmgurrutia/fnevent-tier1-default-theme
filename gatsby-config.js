6e609996ee9e5f173225c00d84c73f605c8564a0
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2023-05-30 12:30:18 -0300

    Defers site metadata to @openeventkit/event-site implementation.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,10 +5,6 @@ module.exports = {
   /*flags: {
     DEV_SSR: true
   },*/
-  siteMetadata: {
-    title: "Tier 1 Default",
-    siteUrl: "https://tier-1-default.fnvirtual.app",
-  },
   plugins: [
     "@openeventkit/event-site"
   ]
