2bd29d2ba80f87371031a1590fceb875d121a812
sebastian marcet <smarcet@gmail.com>
2023-11-03 17:02:17 -0300

    Update index.js

--- src/@openeventkit/event-site/cms/config/index.js
+++ src/@openeventkit/event-site/cms/config/index.js
@@ -1,7 +1,5 @@
 import defaultConfig from "@openeventkit/event-site/src/cms/config";
 
 const config = { ...defaultConfig };
-// TODO: move to env var
-config.backend.repo = "fntechgit/fnevent-tier1-default-theme";
 
 export default config;
